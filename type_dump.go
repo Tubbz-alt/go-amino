5e18ee573dab00d7e9b998f49f562fdc0128f036
Aaron Craelius <aaronc@users.noreply.github.com>
2019-10-24 22:08:55 -0400

    WIP on type dump

--- type_dump.go
+++ type_dump.go
@@ -1,83 +1,85 @@
 package amino
 
-import "reflect"
+import (
+	"fmt"
+	"reflect"
+)
 
 type TypesJson struct {
-	ConcreteTypes  []TypeJson `json:"concrete_types"`
-	InterfaceTypes []TypeJson `json:"interface_types"`
+	ConcreteTypes  []ConcreteJson  `json:"concrete_types"`
+	InterfaceTypes []InterfaceJson `json:"interface_types"`
+}
+
+type ConcreteJson struct {
+	*TypeJson
+	Fields []StructFieldJson
+}
+
+type InterfaceJson struct {
+	Name              string   `json:"name"`
+	ImplementingTypes []string `json:"implementing_types"`
 }
 
 type TypeJson struct {
-	// GoName corresponds to the reflect.Type Name()
-	GoName string `json:"go_name"`
-	// AminoName corresponds to the name passed to RegisterConcrete for concrete types
-	AminoName string       `json:"amino_name"`
-	PkgPath   string       `json:"pkg_path"`
-	String    string       `json:"string"`
-	Kind      reflect.Kind `json:"kind"`
-	// ImplementingTypes lists the amino names of concrete types implementing
-	// this interface for interface types
-	//ImplementingTypes []string          `json:"implementing_types"`
-	Fields []StructFieldJson `json:"fields"`
-	Key    TypeJson          `json:"key"`
-	Elem   TypeJson          `json:"elem"`
-	Len    int               `json:"len"`
+	Name string       `json:"name"`
+	Kind reflect.Kind `json:"kind"`
+	Key  *TypeJson    `json:"key"`
+	Elem *TypeJson    `json:"elem"`
+	Len  int          `json:"len"`
 }
 
 type StructFieldJson struct {
-	Name             string `json:"name"`
-	ProtoFieldNumber uint32 `json:"proto_field_number"`
-	Type             TypeJson
+	Name             string   `json:"name"`
+	ProtoFieldNumber uint32   `json:"proto_field_number"`
+	Type             TypeJson `json:"type"`
 }
 
 type reflector struct {
+	aminoNames map[string]string
+}
+
+func typeKey(typ reflect.Type) string  {
+	return fmt.Sprintf("%s/%s", typ.PkgPath(), typ.Name())
 }
 
 func (r *reflector) findAminoName(typ reflect.Type) string {
-	panic("TODO")
+	return r.aminoNames[typeKey(typ)]
 }
 
-func (r *reflector) storeAminoName(aminoName string, typ reflect.Type) {
-	panic("TODO")
+func (r *reflector) storeAminoName(aminoName string, ti TypeInfo) {
+	r.aminoNames[typeKey(ti.Type)] = aminoName
 }
 
-func (r *reflector) typeToJson(aminoName string, typ reflect.Type) TypeJson {
-	if aminoName == "" {
-		aminoName = r.findAminoName(typ)
-	} else {
-		r.storeAminoName(aminoName, typ)
-	}
+func (r *reflector) typeToJson(typ reflect.Type) *TypeJson {
 	kind := typ.Kind()
-	if kind == reflect.Ptr {
-		return r.typeToJson(aminoName, typ.Elem())
-	}
-	tj := TypeJson{
-		AminoName: aminoName,
-		GoName:    typ.Name(),
-		PkgPath:   typ.PkgPath(),
-		String:    typ.String(),
-		Kind:      kind,
+	tj := &TypeJson{
+		Name: r.findAminoName(typ),
+		Kind: kind,
 	}
 	if kind == reflect.Array || kind == reflect.Slice || kind == reflect.Map {
-		tj.Elem = r.typeToJson("", typ.Elem())
+		tj.Elem = r.typeToJson(typ)
+	}
+	if kind == reflect.Array {
+		tj.Len = typ.Len()
 	}
 	if kind == reflect.Map {
-		tj.Key = r.typeToJson("", typ.Key())
-
-	}
-	if kind == reflect.Struct {
-		n := typ.NumField()
-		for i := 0; i < n; i++ {
-			f := typ.Field()
-			sj := StructFieldJson{Name:}
-			tj.Fields = append(tj.Fields, )
-		}
-	}
-	if kind == reflect.Chan || kind == reflect.Func {
-		panic("unexpected")
+		tj.Key = r.typeToJson(typ)
 	}
 	return tj
 }
 
+func (r *reflector) concreteToJson(ti TypeInfo) ConcreteJson {
+	cj := ConcreteJson{TypeJson: r.typeToJson(ti.Type)}
+	name := ti.Name
+	cj.Name = name
+	if cj.Kind == reflect.Struct {
+
+	}
+	return cj
+}
+
 func (cdc *Codec) DumpTypes() {
+	// register concrete amino names first!
+	// dump types
+	// dump interfaces
 }
